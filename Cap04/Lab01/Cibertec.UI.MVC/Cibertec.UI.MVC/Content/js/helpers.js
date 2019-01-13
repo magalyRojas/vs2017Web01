c9460d10829b38f0464d0aa82dbb3586b5c4f402
magalyRojas <malagita@gmail.com>
2019-01-13 12:20:58 -0500

    semana 4

---
+++ Cap04/Lab01/Cibertec.UI.MVC/Cibertec.UI.MVC/Content/js/helpers.js
@@ -0,0 +1,35 @@
+﻿(function (cibertec) {
+    cibertec.helpers =
+        {
+            replaceAll: function (string, find, replace) {
+                var result = string.replace(
+                    new RegExp(find, 'g'), replace
+                );
+                return result;
+
+            },
+            getAniosArray: function (anioInicio) {
+                var hoy = new Date();
+                var anios = [];
+                for (i = anioInicio; i <= hoy.getFullYear(); i++) {
+                    anios.push(i);
+                }
+                return anios;
+        },
+            BloquearControls: function () {
+                $("input,select,textarea").attr("disabled", "disabled");
+                $("#btnGrabar").attr("disabled", "disabled");
+        },
+        DesbloquearControls: function () {
+            $("input,select,button,textarea").removeAttr("disabled");
+        },
+        SetObjectLocalStorage: function (jsObject, keyStorage) {
+            var json = JSON.stringify(jsObject);
+            localStorage.setItem(keyStorage, json);
+        },
+        GetObjectLocalStorage: function (keyStorage) {
+            var json = localStorage.getItem(keyStorage);
+            return JSON.parse(json);
+        }
+        }
+})(window.cibertec = window.cibertec || {});
